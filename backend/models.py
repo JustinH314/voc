db7728a4c066ae8428f19cd1069c9ef3f2a31a2a
Justin <101justinbii@gmail.com>
2023-10-09 10:27:08 +0800

    feat: use utc zero in timestamp

--- backend/models.py
+++ backend/models.py
@@ -1,10 +1,10 @@
 from pydantic import Field, BaseModel
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 class TermModel(BaseModel):
     id: str = Field(default_factory=uuid.uuid4)
     value: str = Field()
     part_of_speech: str = Field(default="noun")
-    created_at: datetime = Field(default_factory=datetime.now)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
